065278a42e58e4e1854b5a80a99ee89333efca23
8970979 <65878917+8970979@users.noreply.github.com>
2026-08-14 17:27:06 -0300

    Add basic calculator functionality in index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'Operacao.php';
+
+
+if (isset($_POST['operacao'])) {
+
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    $operacao = $_POST['operacao'];
+
+    $calculadora = new Operacao();
+    $calculadora->setValor1($valor1);
+    $calculadora->setValor2($valor2);
+
+    if ($operacao == "somar") {
+        $resultado = $calculadora->somar();
+    } else if ($operacao == "subtrair") {
+        $resultado = $calculadora->subtrair();
+    } else if ($operacao == "multiplicar") {
+        $resultado = $calculadora->multiplicar();
+    } else if ($operacao == "dividir") {
+        $resultado = $calculadora->dividir();
+    } else if ($operacao == "exponenciar") {
+        $resultado = $calculadora->exponenciar();
+    }
+}
+
+require_once 'Tela.php';
+?>
