27fe0abd237ad2e53c18018cd7d210f3f9deb1b4
8970979 <65878917+8970979@users.noreply.github.com>
2026-08-14 17:28:02 -0300

    Add basic calculator HTML form in Tela.php

---
+++ Tela.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    <form method="post" action="index.php">
+  <input type="number" name="valor1" required><br>
+  <input type="number" name="valor2" required><br><br>
+  <button name="operacao" value="somar">Somar</button>
+  <button name="operacao" value="subtrair">Subtrair</button>
+  <button name="operacao" value="multiplicar">Multiplicar</button>
+  <button name="operacao" value="dividir">Dividir</button>
+  <button name="operacao" value="exponenciar">Exponenciar</button>
+</form>
+
+</body>
+</html>
